d274cc898d6aae24aa4975a91d21c9128383e7f4
Santiago Sanchez <ssg5670@gmail.com>
2021-04-15 15:59:12 -0500

    Solicitudes

--- cliente.py
+++ cliente.py
@@ -5,7 +5,10 @@ Created on Thu Apr 15 14:40:30 2021
 @author: 000420606, 000416688, 000437946
 """
 import socket
-
+import random as rd
+import time 
+import numpy as np 
+import matplot.pyplot as plt
 
 
 #create a socket object
@@ -34,3 +37,18 @@ s.close()
 
 
 print('\b El tiempo recibido desde el servidor es %s' % tm.decode('ascii'))
+
+promedio = 8.0
+
+N = 1000
+
+solicitud = [0]
+
+for k in range(N):
+	valor = rd.expovariate(1.0/promedio)
+	solicitud.append(valor)
+
+plt.hist(solicitud)
+np.mean(solicitud)
+
+solicitud[-1]
