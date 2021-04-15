24aa28d82104f675cdecce1fdc1dcbb95088caf0
Pablo Loaiza <pablo@loaizas.com>
2021-04-15 16:48:16 -0500

    Evaluacion acceso id cliente

--- servidor.py
+++ servidor.py
@@ -6,6 +6,13 @@ Created on Thu Apr 15 14:40:30 2021
 """
 
 import socket
+import numpy as np
+
+usuarioPermitidos = []
+permitirAcceso = False
+aforo = 100
+idCliente = 53
+posicion = 0
 
 #create a socket object
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,5 +26,20 @@ serversocket.bind((host, port))
 
 #queue up to 5 requests
 serversocket.listen(5)
+
+#usuarios permitidos
+for k in range(aforo):
+    usuarioPermitidos.append(np.random.randint(0, 119))
+    
+
+while(posicion < 100):
+    if(idCliente == usuarioPermitidos[posicion]):
+        permitirAcceso = True
+    else:
+        posicion += 1
+    break
+
+       
+       
 
 print('--DESCONECTADO--')
