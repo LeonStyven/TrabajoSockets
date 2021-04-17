de2a4e2891b17f3fe4b7e06db007b081ac96eaf1
LeonStyven <j.stca03@gmail.com>
2021-04-16 20:26:42 -0500

    Depuracion y comentacion del codigo y la finalizacion del trabajo

--- cliente.py
+++ cliente.py
@@ -2,65 +2,68 @@
 """
 Created on Thu Apr 15 14:40:30 2021
 
-@author: 000420606, 000416688, 000437946
+Autores:
+
+Julian Styven Colorado Agudelo - 000420606
+Pablo Loaiza Mejia - 000416688
+Santiago Sanchez Giraldo - 000437946
+
 """
+#Importacion de librerias
 import socket
 import random as rd
 import time 
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
-idCliente = 0
-promedio = 1
-solicitud = [0]
-repeticiones = 120
+#NOTA: LA CANTIDAD DE REPETICIONES EN EL CLIENTE, DEBE SER LA MISMA QUE EL NUMERO DE VECES QUE SE EJECUTA EL FOR EN EL SERVIDOR
 
 
+#Declaracion de variables
 
+idCliente = 0           #ID del usuario que ingresa
+promedio = 0.1          #Valor promedio en segundos para el acceso de usuarios
+solicitud = [0]         #Variable que almacena el tiempo que el servidor espera antes de la siguiente consulta
+repeticiones = 120      #Numero de solicitudes al servidor
 
-
-#get local machine name
+#Informcion local para el manejo de la direccion del servidor
 host = socket.gethostname()
 port = 9999
 
 
-
-#Obtener el tiempo entre cada evento
-'''for k in range(N):
-	tiempoEspera = rd.expovariate(1.0/promedio)
-	solicitud.append(tiempoEspera)
-
-plt.hist(solicitud, 100)
-np.mean(solicitud)'''
-
-
-#El servidor espera el tiempo que le indique la variable tiempoEspera
+#Al entrar en el while, el cliente comienza a generar eventos al servidor
 
 while(repeticiones>0):
-    #create a socket object
+    
+    #Creacion del Socket
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
+    #Obtener el ID del cliente de forma aleatoria
     idCliente = rd.randint(0, 119)
     mensaje = idCliente.to_bytes(2, 'big')
     
+    #Obtener tiempo que el servidor debe esperar antes de la siguiente consulta
     tiempoEspera = rd.expovariate(1.0/promedio)
     solicitud.append(tiempoEspera)
     repeticiones -= 1
     
-    #connection to hostname on the port
+    #Conexion con el servidor
     clientsocket.connect((host, port))
-    clientsocket.send(mensaje)
     
-    #receive no more than 1024 bytes
+    #Comunicacion con el servidor
+    clientsocket.send(mensaje)
     permitirAcceso = clientsocket.recv(1024)
     
+    
+    #Según la respuesta del servidor, el programa evalua si permite o no la entrada del usuario
     if (permitirAcceso == bytes(1)):
         print(str(idCliente) + ' ACCESO CONCEDIDO')
-    else:
+    elif(permitirAcceso== bytes(0)):
         print(str(idCliente) + ' ACCESO DENEGADO')
-        
+    else:
+        print(str(idCliente) + ' Saliendo...')
     time.sleep(tiempoEspera)
     
+    #Cerrar la conexion con el servidor
     clientsocket.close()
     
--- servidor.py
+++ servidor.py
@@ -2,60 +2,87 @@
 """
 Created on Thu Apr 15 14:40:30 2021
 
-@author: 000420606, 000416688, 000437946
+Autores:
+
+Julian Styven Colorado Agudelo - 000420606
+Pablo Loaiza Mejia - 000416688
+Santiago Sanchez Giraldo - 000437946
+
 """
 
+#Importacion de librerias
 import socket
 import numpy as np
+import matplotlib.pyplot as plt
 
-usuarioPermitidos = []
-permitirAcceso = 0
-aforo = 100
-adentro = [1, 5, 6]
+#NOTA: LA CANTIDAD DE REPETICIONES EN EL CLIENTE, DEBE SER LA MISMA QUE EL NUMERO DE VECES QUE SE EJECUTA EL FOR EN EL SERVIDOR
 
-#usuarios permitidos por lista de acceso o por posicion
-for k in range(aforo):
-    usuarioPermitidos.append(np.random.randint(0, 119))
+
+
+#Declaracion de variables
+usuariosPermitidos = []         #Lista de IDs de usuarios que pueden entrar
+permitirAcceso = 0              #Variable que determina sin una persona puede o no entrar a la universidad
+aforo = 100                     #Aforo maximo de la U
+adentro = []                    #Listado de IDs de personas dentro de la universidad
+cantidadUsuarios = []           #Numero de usuarios dentro de la Universidad
+
+
+#Generacion de lista de usuarios permitidos = aforo
+while(len(usuariosPermitidos)<aforo):
+    usuarioAleatorio = np.random.randint(0, 119)
+    if(not(usuarioAleatorio in usuariosPermitidos)):
+        usuariosPermitidos.append(usuarioAleatorio)
     
-#create a socket object
+    
+print(usuariosPermitidos)
+    
+#Creacion del socket por parte del servidor
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#get local machine name
+#Obtener dereccion del servidor segun la informacion del propio equipo
 host = socket.gethostname()
 port = 9999
 
-#bind to the port
+
+#Establecer host y puerte en el va a trabajar el servidor
 serversocket.bind((host, port))
 
-#queue up to 5 requests
+#Cola para clientes entrantes
 serversocket.listen(5)
 
-while True:
+#Comunicacion con el cliente
+for k in range(120):
+    
+    #Establecer conexion con el cliente
     clientsocket,addr = serversocket.accept()
     
+    #Comunicacion con cliente
     idCliente = clientsocket.recv(1024)
     mensaje = int.from_bytes(idCliente, 'big')
     
-    if(mensaje in usuarioPermitidos):   
+    #Evaluacion del ID del usuario 
+    if(mensaje in usuariosPermitidos):   
         if (not(mensaje in adentro)):
             permitirAcceso = bytes(1)
-            
             if(len(adentro)<=aforo):
                 permitirAcceso = bytes(1)
+                adentro.append(mensaje)
+                cantidadUsuarios.append(len(adentro))
             else:
-                permitirAcceso = bytes(0) 
-         
+                permitirAcceso = bytes(0)
         else: 
-            permitirAcceso = bytes(0)
+            permitirAcceso = bytes(2)
+            adentro.remove(mensaje)
+            cantidadUsuarios.append(cantidadUsuarios[-1]-1)   
     else: 
         permitirAcceso = bytes(0)
         
-    #Evaluacion del Aforo
-
-    
+    #Enviar el resultado de la evaluacion, y cerrar la conexion
     clientsocket.send(permitirAcceso)
     clientsocket.close()
 
-print('--DESCONECTADO--')
-
+#Grafica de la cantidad de usuarios dentro de la universidad a lo largo del tiempo
+plt.grid(True)
+plt.plot(cantidadUsuarios)
+plt.show()
 
